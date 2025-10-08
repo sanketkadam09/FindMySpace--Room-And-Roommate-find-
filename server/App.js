dd441b5f94bb3482b9937e98f1aec029b328c0c8
Sanket Kadam <sk8185710@gmail.com>
2025-10-08 09:29:38 +0530

    Fixed CORS issue for frontend connection

--- server/App.js
+++ server/App.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: "https://find-my-space-room-and-roommate-fin.vercel.app/",
     credentials: true,
   })
 );
